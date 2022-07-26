2f833326e5cd28b2a40af13d4735fe9990cdec87
mondo84_dell <ydavila@transportationamerica.com>
2022-07-26 11:54:39 -0500

    se agrega proyecto

--- src/api/users/users.controller.ts
+++ src/api/users/users.controller.ts
@@ -1,15 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UseGuards, Request } from '@nestjs/common';
 import { BaseController } from '../commons/controller.commons';
 import { User } from './user.entity';
 import { BaseService } from '../commons/service.commons';
 import { UsersService } from './users.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('api/users')
 export class UsersController extends BaseController<User> {
   constructor(private readonly userService: UsersService) {
     super();
   }
-
+  @UseGuards(AuthGuard('local'))
+  @Post('login')
+  async login(@Request() req: any) {
+    return req.user;
+  }
   getService(): BaseService<User> {
     return this.userService;
   }
--- src/api/users/users.module.ts
+++ src/api/users/users.module.ts
@@ -1,12 +1,14 @@
-import  { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { LocalStrategy } from '../auth/local.strategy';
+import { AuthService } from "../auth/auth.service";
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  providers: [UsersService],
+  providers: [UsersService, AuthService, LocalStrategy],
   controllers: [UsersController],
   exports: [TypeOrmModule],
 })
